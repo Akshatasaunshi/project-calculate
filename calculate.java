5deba5704fd13dc40f1a11f2dd373716094dcfec
Akshata <01fe22bca032@kletech.ac.in>
2024-01-22 18:01:57 +0530

    code changes related to arithmetic ops

---
+++ calculate.java
@@ -0,0 +1,29 @@
+public class calculate
+{
+    public int add(int a,int b)
+    {
+        return a+b;
+    }
+    public int sub(int a,int b)
+    {
+        return a-b;
+    }
+    public int multi(int a,int b)
+    {
+        return a*b;
+    }
+    public int div(int a,int b)
+    {
+        return a/b;
+    }
+}
+class result
+{
+    public static void main(String args[]){
+        calculate c=new calculate();
+        System.out.println("Addition="+c.add(5,3));
+        System.out.println("Substraction="+c.sub(7,3));
+        System.out.println("Multiplication="+c.multi(5,3));
+        System.out.println("Division="+c.div(10,5));
+    }
+}
