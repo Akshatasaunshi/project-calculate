e2f03a6ad0a7e9580fb436371f760ee4fb094925
Akshatasaunshi <01fe22bca032@kletech.ac.in>
2024-01-24 20:46:08 +0530

    code realted to largest number

--- largest_number1.java
+++ largest_number1.java
@@ -1,31 +1,21 @@
-import java.util.Scanner;
-
-public class largest_number1{
-
+class FindLargestNumber {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    
+        int num1 = 25;
+        int num2 = 42;
+        int num3 = 18;
 
-        System.out.println("Enter the first number: ");
-        double num1 = scanner.nextDouble();
-
-        System.out.println("Enter the second number: ");
-        double num2 = scanner.nextDouble();
-
-        System.out.println("Enter the third number: ");
-        double num3 = scanner.nextDouble();
-
-        double largestNumber = findLargest(num1, num2, num3);
+        int largestNumber = findLargest(num1, num2, num3);
 
         System.out.println("The largest number is: " + largestNumber);
     }
-
-    public static double findLargest(double num1, double num2, double num3) {
-        if (num1 >= num2 && num1 >= num3) {
-            return num1;
-        } else if (num2 >= num1 && num2 >= num3) {
-            return num2;
+    private static int findLargest(int a, int b, int c) {
+        if (a >= b && a >= c) {
+            return a;
+        } else if (b >= a && b >= c) {
+            return b;
         } else {
-            return num3;
+            return c;
         }
     }
 }
