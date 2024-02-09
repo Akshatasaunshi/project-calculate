973e7d4c41f7ca33ab35e06b29b0d52c6d5e43f7
Akshata <01fe22bca032@kletech.ac.in>
2024-02-09 15:37:30 +0530

    Inbuilt functions

---
+++ mathfunction.java
@@ -0,0 +1,11 @@
+class mathfunction{
+    public static void main(String args[]){
+        System.out.println("Maximum is: "+Math.max(5,9));
+        System.out.println("Minimum is: "+Math.min(4,2));
+        System.out.println("Squareroot is: "+Math.sqrt(25));
+        System.out.println("cube root is: "+Math.cbrt(12));
+        System.out.println("Random : "+Math.random());
+        System.out.println("Absolute is: "+Math.abs(2.5));
+
+}
+}
